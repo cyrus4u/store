ef865fa6a6fc754fffa6dfe4808cd7e0e3a52459
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-15 15:07:25 +0330

    adding marker to map

--- lib/google_maps.dart
+++ lib/google_maps.dart
@@ -17,14 +17,41 @@ class _GoogleMapsState extends State<GoogleMaps> {
       body: FlutterMap(
         // mapController: MapController(),
         options: MapOptions(
-          initialCenter: LatLng(35.699693, 51.338138),
-          initialZoom: 15,
+          initialCenter: LatLng(35.768663, 51.458457),
+          initialZoom: 5,
         ),
         children: [
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.example.store',
             // + many other options
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(35.768663, 51.458457),
+                width: 80,
+                height: 80,
+                child: Text(
+                  'نمایندگی دیجی کالا شعبه تهران',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              Marker(
+                point: LatLng(37.278906, 49.584580),
+                width: 80,
+                height: 80,
+                child: Text('نمایندگی دیجی کالا شعبه رشت',
+                  style: TextStyle(color: Colors.red),),
+              ),
+              Marker(
+                point: LatLng(31.315439, 48.663467),
+                width: 80,
+                height: 80,
+                child: Text('نمایندگی دیجی کالا شعبه اهواز',
+                  style: TextStyle(color: Colors.red),),
+              ),
+            ],
           ),
         ],
       ),
